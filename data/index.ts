152c1c05cbbb08c34c0571e40ed9fa610a3e8d90
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2025-02-17 15:39:54 +0300

    first commit

---
+++ data/index.ts
@@ -0,0 +1,230 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Experience", link: "#experience" },
+  { name: "Approach", link: "#approach" },
+  // { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" }
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I emphasize seamless collaboration and clear communication with clients",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/communication.jpg",
+    spareImg: ""
+  },
+  {
+    id: 2,
+    title: "I'm highly flexible with time zone communication",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: ""
+  },
+  {
+    id: 3,
+    title: "My Tech Stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: ""
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg"
+  },
+
+  {
+    id: 5,
+    title: "Currently Working on StudyHero App",
+    description: "Whats New",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg"
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: ""
+  }
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com"
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com"
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com"
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com"
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Mahan was a fantastic experience. His technical expertise, problem-solving skills, and commitment to delivering high-quality solutions made a huge impact on our project. His attention to detail and proactive approach set him apart. I highly recommend him for any development needs.",
+    name: "Adison Dorwart",
+    title: "CTO"
+  },
+  {
+    quote:
+      "Mahan is a top-tier developer with a deep understanding of modern technologies. He not only met our expectations but exceeded them with his innovative solutions and efficient workflow. His ability to simplify complex problems is remarkable. We couldn't have asked for a better collaborator.",
+    name: "Rayna Workman",
+    title: "Founder"
+  },
+  {
+    quote:
+      "Mahan's expertise and dedication were invaluable to our project's success. He has a keen eye for detail, writes clean and efficient code, and always delivers on time. His collaborative nature makes him a pleasure to work with. Highly recommended!",
+    name: "Carla Saris",
+    title: "Engineering Manager"
+  },
+  {
+    quote:
+      "Mahan is an exceptional developer who truly understands how to create seamless and scalable solutions. His professionalism and ability to quickly adapt to challenges made a significant difference in our workflow. We look forward to working with him again.",
+    name: "Isabella Hayes",
+    title: "Product Lead"
+  },
+  {
+    quote:
+      "Mahan brings a perfect blend of technical skill and creativity to every project. His ability to transform complex ideas into functional, elegant solutions is impressive. If you're looking for a developer who truly cares about your vision, Mahan is the right choice.",
+    name: "Noah Fitzgerald",
+    title: "CEO"
+  }
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Stack Mobile Developer - StudyHero",
+    desc: "Designed, Developed and Deployed a mobile application for both iOS & Android platforms using Flutter",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg"
+  },
+  {
+    id: 2,
+    title: "Full Stack Mobile Developer - Freelance",
+    desc: "Designed, Developed and Deployed mobile apps for both iOS & Android platforms using Flutter.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg"
+  },
+  {
+    id: 3,
+    title: "Mobile Developer - Singularity",
+    desc: "Contributed to a large-scale mobile app project, available on both the App Store and Google Play. I optimized its performance, enhanced functionality, and improved the overall user experience.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg"
+  },
+  {
+    id: 4,
+    title: "Full Stack Business Analyst - Singularity",
+    desc: "Designed intuitive mobile and web UIs, improved usability with comprehensive testing, and collaborated across teams to enhance product quality in an agile environment.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg"
+  },
+  {
+    id: 5,
+    title: "User Experience Designer - Singularity",
+    desc: "Designed intuitive mobile and web UIs and collaborated across teams to enhance product quality in an agile environment.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg"
+  },
+
+  {
+    id: 6,
+    title: "Tech Team Member - Google Developer Students Club",
+    desc: "Advanced from active member to Core Tech team leader, mentoring peers, leading Python workshops, and organizing tech events.",
+    className: "md:col-span-2",
+    thumbnail: "/google.png"
+  },
+
+  {
+    id: 7,
+    title: "AI Research Laboratory Assistant",
+    desc: "Assisted the AI research team by translating tutorials and creating interactive videos, improving accessibility and knowledge sharing.",
+    className: "md:col-span-4",
+    thumbnail: "/exp3.svg"
+  }
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/Linkedin.svg",
+    link: "https://www.linkedin.com/in/mahankheirollahi/"
+  },
+  {
+    id: 2,
+    img: "/Github.svg",
+    link: "https://github.com/itsthemoony"
+  },
+  {
+    id: 3,
+    img: "/Medium.svg",
+    link: "https://medium.com/@itsthemoony"
+  },
+
+  {
+    id: 4,
+    img: "/Instagram.svg",
+    link: "https://www.instagram.com/itsthemoony"
+  },
+
+  {
+    id: 5,
+    img: "/Tiktok.svg",
+    link: "https://www.tiktok.com/@itsthemoony"
+  },
+  {
+    id: 6,
+    img: "/Youtube.svg",
+    link: "https://www.youtube.com/@itsthemoony"
+  }
+];
